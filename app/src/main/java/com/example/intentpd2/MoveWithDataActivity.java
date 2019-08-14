43866ced882e4ede8115811abb3229ce78b1d6c2
Azriel <bancet6996@gmail.com>
2019-08-14 11:24:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentpd2/MoveWithDataActivity.java
@@ -0,0 +1,24 @@
+package com.example.intentpd2;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+    public static String EXTRA_AGE = "extra_age";
+    public static String EXTRA_NAME = "extra_name";
+    private TextView tvDataReceived;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+        tvDataReceived = (TextView) findViewById(R.id.tv_data_received);
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+        String text = "Name : " +name+", Your Age : "+age;
+        tvDataReceived.setText(text);
+    }
+}
